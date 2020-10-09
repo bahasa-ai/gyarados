f84707b76253dc4194d800d58e48d72995988139
M Gilang Januar <mgilangjanuar@gmail.com>
2020-10-09 15:20:14 +0700

    init server client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hello')
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.get('/', (_: Request, res: Response) => res. send({ 'alive?': true }))
+
+app.listen(process.env.PORT || '6606', () => console.log(`App started in http://localhost:${process.env.PORT || 6606}`))
