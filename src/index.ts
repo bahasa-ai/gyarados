af75fb1a26c18481936f61e80c5fa173cca86678
M Gilang Januar <mgilangjanuar@gmail.com>
2020-10-09 15:37:38 +0700

    propose folder structure for app and all main pipelines

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import express, { Request, Response } from 'express'
-
-const app = express()
-
-app.get('/', (_: Request, res: Response) => res. send({ 'alive?': true }))
-
-app.listen(process.env.PORT || '6606', () => console.log(`App started in http://localhost:${process.env.PORT || 6606}`))
+import './App'
